8459518b841b1b472c5b1dd348022100679490ad
3pns <samuel.bohn@etu.unistra.fr>
2017-02-10 11:02:11 +0100

    fixes plaçage d'une pièce

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	_ "bufio"
 	_ "strings"
 	"encoding/json"
+	"strconv"
 )
 
 
@@ -61,7 +62,7 @@ func main() {
 	//pièces
 	fmt.Println("----- Piece -----")
 	fmt.Println(board.Pieces[0])
-	board.placePiece(&board.Pieces[0], Square{10, 3, true})
+	board.placePiece(&board.Pieces[4], Square{10, 3, true})
 
 	fmt.Println("----- PRINT TO JSON -----")
 	//b, err := json.Marshal(Square{0, 0, true})
@@ -111,10 +112,46 @@ func initPieces(board *Board){
 
 	var piece Piece
 	piece.Id = 0
-	piece.Rotation = "N"
+	piece.Rotation = "S"
 	piece.Flipped = false
 	piece.Cubes = append(piece.Cubes, Cube{0, 0})
 	board.Pieces = append(board.Pieces, piece)
+
+	var piece1 Piece
+	piece1.Id = 1
+	piece1.Rotation = "S"
+	piece1.Flipped = false
+	piece1.Cubes = append(piece1.Cubes, Cube{0, 0})
+	piece1.Cubes = append(piece1.Cubes, Cube{0, 1})
+	board.Pieces = append(board.Pieces, piece1)
+
+	var piece2 Piece
+	piece2.Id = 2
+	piece2.Rotation = "S"
+	piece2.Flipped = false
+	piece2.Cubes = append(piece2.Cubes, Cube{0, 0})
+	piece2.Cubes = append(piece2.Cubes, Cube{0, 1})
+	piece2.Cubes = append(piece2.Cubes, Cube{1, 0})
+	board.Pieces = append(board.Pieces, piece2)
+
+	var piece3 Piece
+	piece3.Id =3
+	piece3.Rotation = "S"
+	piece3.Flipped = false
+	piece3.Cubes = append(piece3.Cubes, Cube{0, 0})
+	piece3.Cubes = append(piece3.Cubes, Cube{0, 1})
+	piece3.Cubes = append(piece3.Cubes, Cube{0, 2})
+	board.Pieces = append(board.Pieces, piece3)
+
+	var piece4 Piece
+	piece4.Id =3
+	piece4.Rotation = "S"
+	piece4.Flipped = false
+	piece4.Cubes = append(piece4.Cubes, Cube{0, 0})
+	piece4.Cubes = append(piece4.Cubes, Cube{1, 0})
+	piece4.Cubes = append(piece4.Cubes, Cube{0, 1})
+	piece4.Cubes = append(piece4.Cubes, Cube{0, 2})
+	board.Pieces = append(board.Pieces, piece4)
 
 	fmt.Println("pieces generated with success !\n")
 }
@@ -122,6 +159,37 @@ func initPieces(board *Board){
 func (board Board) placePiece(piece *Piece, square Square) {
 	board.Squares[square.X][square.Y].Empty = false
 	piece.Origin = board.Squares[square.X][square.Y]
+	piece.Flipped = true
+	piece.Rotation = "N"
+
+	//postionnement de la pièce dans l'espace
+
+
+	//1 - vérifier si on a le droit de placer la pièce
+	//2 - placer la pièce
+	fmt.Println("----- Plaçage d'une pièce -----")
+	for _,cube := range piece.Cubes {
+		var xFactor = 1
+		var yFactor = 1
+		var xBoardValue = cube.X
+		var yBoardValue = cube.Y
+		if piece.Flipped {
+			xFactor = -xFactor
+		}
+		fmt.Println("Avant : x :" + strconv.Itoa(xBoardValue) + ", y: " + strconv.Itoa(yBoardValue))
+		if piece.Rotation == "W" {
+			xBoardValue = -cube.Y
+			yBoardValue = -cube.X
+		} else if piece.Rotation == "N" {
+			xBoardValue = -cube.X
+			yBoardValue = -cube.Y
+		} else if piece.Rotation == "E" {
+			xBoardValue = cube.Y
+			yBoardValue = cube.X
+		}
+			fmt.Println("Apres : x :" + strconv.Itoa(xBoardValue) + ", y: " + strconv.Itoa(yBoardValue))
+			board.Squares[square.X + xFactor * xBoardValue][square.Y + yFactor * yBoardValue].Empty = false
+		}
 }
 
 func (player *Player) initPieces() {
