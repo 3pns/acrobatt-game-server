e4adb5e8276a65b2f66246e1daddd2f623cd069e
Samuel BOHN <samuel.bohn@etu.unistra.fr>
2017-05-08 17:51:29 +0200

    update AuthenticateJson access-token to access_token

--- jsonapi/authenticate.go
+++ jsonapi/authenticate.go
@@ -2,6 +2,6 @@ package jsonapi
 
 type AuthenticateJson struct {
   PlayerId int `json:"player_id"`
-  AccessToken string `json:"access-token"`
+  AccessToken string `json:"access_token"`
   Client string `json:"client"`
 }
