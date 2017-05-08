7a9fc5cb9ff1d3a9c72162ccaec86a29156aba2a
Samuel BOHN <samuel.bohn@etu.unistra.fr>
2017-05-08 19:02:49 +0200

    changed access_token to access-token

--- jsonapi/authenticate.go
+++ jsonapi/authenticate.go
@@ -2,6 +2,6 @@ package jsonapi
 
 type AuthenticateJson struct {
   PlayerId int `json:"player_id"`
-  AccessToken string `json:"access_token"`
+  AccessToken string `json:"access-token"`
   Client string `json:"client"`
 }
